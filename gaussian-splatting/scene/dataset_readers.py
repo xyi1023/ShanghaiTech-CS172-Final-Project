7f9e10d4998a779b73c19d873be2dbd2f9aed284
xyi1023 <hongyu@shanghaitech.edu.cn>
2023-12-19 15:39:57 +0800

    read our data

--- gaussian-splatting/scene/dataset_readers.py
+++ gaussian-splatting/scene/dataset_readers.py
@@ -476,15 +476,13 @@ def readOursNGPInfo(path, white_background, eval, extension=".png"):
 
     ply_path = os.path.join(path, "points3d.ply")
     if not os.path.exists(ply_path):
-        # Since this data set has no colmap data, we start with random points
         num_pts = 100_000
         print(f"Generating random point cloud ({num_pts})...")
-
-        # We create random points inside the bounds of the synthetic Blender scenes
         xyz = np.random.random((num_pts, 3)) * 2.6 - 1.3
         shs = np.random.random((num_pts, 3)) / 255.0
         pcd = BasicPointCloud(points=xyz, colors=SH2RGB(
             shs), normals=np.zeros((num_pts, 3)))
+
         storePly(ply_path, xyz, SH2RGB(shs) * 255)
     try:
         pcd = fetchPly(ply_path)
